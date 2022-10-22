6ff2d21d1ecdc24ea62e85744437afb6647d322b
Mariano Altieri <marianoa@givingassistant.org>
2022-10-22 10:58:19 -0300

    Added websocket to Coin view

--- src/components/views/Coins.tsx
+++ src/components/views/Coins.tsx
@@ -1,37 +1,33 @@
-import React, { useMemo, useRef, useState } from 'react'
+import React, { useMemo, useState, useEffect } from 'react'
 import cx from 'classnames'
 import millify from 'millify'
 import { useQuery } from 'react-query'
 import { GoArrowDown, GoArrowUp } from 'react-icons/go'
 
 import { fetchCoins } from '../../services/coins'
-import { useEffect } from 'react'
-
-const isAssetGrowing = (asset: any) => {
-    if (!asset?.previousPriceUsd) {
-        return null
-    }
-
-    return parseFloat(asset.priceUsd) > parseFloat(asset.previousPriceUsd)
-}
 
 const Coins = () => {
-    const { status, data } = useQuery(['getCoins'], fetchCoins, {
-        //refetchInterval: 1000,
-    })
+    const { status, data } = useQuery(['getCoins'], fetchCoins)
     const { data: assets = [] } = data ?? {}
-    const oldAssets = useRef<any>([])
+    const [socketAssets, setSocketAssets] = useState<any>([])
     const [search, setSearch] = useState('')
 
     const formattedAssets = useMemo(() => {
         let output = assets.map((asset: any, index: number) => {
-            if (!oldAssets.current[index]) {
-                oldAssets.current[index] = { ...asset }
-                return asset
-            }
+            const newAsset = { ...asset }
+            Object.keys(socketAssets).forEach((key: string) => {
+                if (key === newAsset.id) {
+                    const oldValue = parseFloat(newAsset.priceUsd).toFixed(3)
+                    const newValue = parseFloat(socketAssets[key]).toFixed(3)
 
-            asset.previousPriceUsd = oldAssets.current[index].priceUsd
-            return asset
+                    newAsset.isGrowing = newValue > oldValue
+                    newAsset.isShrinking = newValue < oldValue
+
+                    newAsset.priceUsd = parseFloat(socketAssets[key])
+                }
+            })
+
+            return newAsset
         })
 
         if (search) {
@@ -45,17 +41,20 @@ const Coins = () => {
         }
 
         return output
-    }, [assets, search])
+    }, [assets, search, socketAssets])
 
     useEffect(() => {
+        const ids = assets.map((asset: any) => asset.id).join(',')
         const pricesWs = new WebSocket(
-            'wss://ws.coincap.io/prices?assets=bitcoin,ethereum,monero,litecoin'
+            `wss://ws.coincap.io/prices?assets=${ids}`
         )
 
-        pricesWs.onmessage = function (msg) {
-            console.log(msg.data)
+        pricesWs.onmessage = (e: MessageEvent) => {
+            setSocketAssets(JSON.parse(e.data))
         }
-    }, [])
+
+        return () => pricesWs.close()
+    }, [assets])
 
     if (status === 'loading') return <h1>Loading...</h1>
 
@@ -97,12 +96,7 @@ const Coins = () => {
                 </thead>
                 <tbody className="divide-y divide-ce-purple-700 text-md text-left bg-ce-purple-400 text-black rounded-lg">
                     {formattedAssets.map((asset: any) => (
-                        <tr
-                            key={asset.id}
-                            className={cx({
-                                flash: isAssetGrowing(asset),
-                            })}
-                        >
+                        <tr key={asset.id}>
                             <td className="whitespace-nowrap px-3 py-2 capitalize w-[30px]">
                                 {asset.rank}
                             </td>
@@ -122,10 +116,10 @@ const Coins = () => {
                             <>
                                 <td
                                     className={cx({
-                                        'whitespace-nowrap px-3 py-2': true,
-                                        'asset-growing': isAssetGrowing(asset),
-                                        'asset-shrinking':
-                                            isAssetGrowing(asset) === false,
+                                        'whitespace-nowrap px-3 py-2 w-[150px]':
+                                            true,
+                                        'asset-shrinking': asset.isShrinking,
+                                        'asset-growing': asset.isGrowing,
                                     })}
                                 >
                                     <div className="flex items-center">
